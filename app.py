17154cdbc97a071a2f4324fdbb771bef7cbee296
rashedevs <rasheduap2015@gmail.com>
2024-10-06 21:08:41 +0600

    add dockerfile

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def test_flask():
-    return "Running my first flask app"
+    return "Flask app is running from kubernetes"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=5000)
